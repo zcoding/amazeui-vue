e2bebe8898f383c28207392df0b580618af4ff26
zcoding <wuzijie_1991@163.com>
2015-10-29 17:03:13 +0800

    slider init

---
+++ src/slider/slider.vue
@@ -0,0 +1,49 @@
+<template>
+
+<div class="am-slider am-slider-default">
+  <div class="am-viewport">
+    <ul class="am-slides">
+      <li class="clone" v-repeat="item in list">
+        <img v-attr="src: item.img">
+        <div class="am-slider-desc" v-if="item.text">{{ item.text }}</div>
+      </li>
+    </ul>
+  </div>
+  <ol class="am-control-nav am-control-paging">
+    <li v-repeat="list"><a href="#" v-class="am-active: $index === current">{{ $index+1 }}</a></li>
+  </ol>
+  <ul class="am-direction-nav">
+    <li class="am-nav-prev"><a class="am-prev" href="#"> </a></li>
+    <li class="am-nav-next"><a class="am-next" href="#"> </a></li>
+  </ul>
+</div>
+
+</template>
+
+<script>
+
+module.exports = {
+
+  ready: function() {
+    // initialize
+  },
+
+  props: {
+    list: {
+      type: Array,
+      default: function() {
+        return [];
+      },
+      required: true
+    }
+  },
+
+  data: function() {
+    return {
+      current: 0
+    };
+  }
+
+};
+
+</script>
